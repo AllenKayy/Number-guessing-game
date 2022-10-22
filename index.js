69398a79c780c5076a108622afed7ac8cda1d75d
Allen <99987974+AllenKayy@users.noreply.github.com>
2022-10-22 04:05:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let userName, message;
+  
+userName = prompt("Please input your username");
+
+console.log(`Hi ${userName}, Welcome to my number number guessing game!`);
+
+message = "Today, i will think of a number, and you will predict it. You have 3 lives and for each correct pediction, you will move up a level."
+
+console.log(message);
+
+const popUp = prompt("press enter..");
